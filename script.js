d0b37158a7cadd7ca902b2a257d2826e196fe6b6
logannzl <loganoverwater@gmail.com>
2022-12-08 20:45:10 +1300

    Added limit for number of squares to be max 100. Also a min of 1.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('#button');
+const buttonSetup = document.querySelector('#buttonSetup');
 const container = document.querySelector('#container')
 
 container.style.cssText = 'display: flex';
@@ -14,27 +14,48 @@ function createSquare(parent){
     square.style.cssText = `width: ${sizeStr}px; height: ${sizeStr}px`; 
     parent.appendChild(square);
     square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'blue';
-        });
+        square.style.backgroundColor = "blue";
+    });
+
 }
+
 
 function createRow(){
     const row = document.createElement('div');
     row.classList.add('row');
     container.appendChild(row);
-    console.log(createSquare);
     for(let i = 0; i < numSquares; i++){
         createSquare(row);
     }
 }
 
-button.addEventListener('click', () => {
-    numSquaresStr = '';
-    numSquaresStr = prompt('Enter the number of squares you would like per side.');
-    numSquares = Number(numSquaresStr);
-    size = (700 - (numSquares * 2)) / numSquares;
-    sizeStr = size.toString();
-    for(let x = 0; x < numSquares; x++){
-        createRow();
+function clearGrid(gridSize){
+    for (y = 0; y < gridSize; y++){
+        container.removeChild(container.lastChild);
     }
+        numSquares = 0;
+    }
+
+
+
+buttonSetup.addEventListener('click', function draw() {
+    if (numSquares > 0 && numSquares < 101){
+        clearGrid(numSquares);
+        draw();
+    }
+    else{
+    numSquares = 0;
+    numSquaresStr = prompt('Enter the number of squares you would like per side. 1 - 100');
+    numSquares = Number(numSquaresStr);
+        if (numSquares < 1 || numSquares > 100){
+            draw ();
+        }
+        else{
+            size = (700 - (numSquares * 2)) / numSquares;
+            sizeStr = size.toString();
+            for(let x = 0; x < numSquares; x++){
+                createRow();
+            }
+    }}
+
 });
